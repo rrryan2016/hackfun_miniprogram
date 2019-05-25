f5539f4baf0959f08f110e761f10e0baf30f9a24
ghostInSh3ll <15273152851@163.com>
2019-05-25 09:25:03 +0800

    swiper added
    增加了滑动图片栏

--- pages/right/right.js
+++ pages/right/right.js
@@ -1,3 +1,21 @@
 Page({
+	data:{
+		imgUrls:[
+		'/images/exp/harden1.jpg',
+		'/images/exp/harden2.jpg',
+		'/images/exp/harden3.jpg',
+		'/images/exp/graffiti_1.jpg',
+		'/images/exp/graffiti_2.jpg',
+		'/images/exp/graffiti_3.jpg',
+		'/images/exp/graffiti_4.jpg',
+		'/images/exp/graffiti_5.jpg',
+		'/images/exp/graffiti_6.jpg',
+		'/images/exp/graffiti_7.jpg',
+		'/images/exp/graffiti_8.jpg',
+		]
+	},
+	toupper:function(){
+		console.log("Toupper Triggered")
+	}
   
 })
